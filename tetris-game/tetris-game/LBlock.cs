e91547b921b50988af0791b263bea648217aca6f
Sebastian Majcher <SebastianMajcher2005@gmail.com>
2022-05-14 12:50:52 +0200

    Update LBlock.cs

--- tetris-game/tetris-game/LBlock.cs
+++ tetris-game/tetris-game/LBlock.cs
@@ -10,9 +10,9 @@ namespace tetris_game
     {
         public override int Id => 3;
 
-        protected override Position StartOffset => new(0, 3);
+        Position StartOffset => new(0, 3);
 
-        protected override Position[][] Tiles => new Position[][] {
+        Position[][] Tiles => new Position[][] {
             new Position[] {new(0,2), new(1,0), new(1,1), new(1,2)},
             new Position[] {new(0,1), new(1,1), new(2,1), new(2,2)},
             new Position[] {new(1,0), new(1,1), new(1,2), new(2,0)},
